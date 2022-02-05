a19a4d103d7601725d9c38ea377fd751fdb37a2d
gitcharcha <devopscharcha@gmail.com>
2022-02-05 17:56:42 +0530

    automation

---
+++ Network.bat
@@ -0,0 +1,4 @@
+@echo off
+Rem This command is to do nslookup of google
+nslookup google.com
+echo "done"
---
+++ filelist.bat
@@ -0,0 +1,4 @@
+@echo off
+Rem This code is to dipslay thr ip address of the system
+ipconfig /all
+echo "done"
