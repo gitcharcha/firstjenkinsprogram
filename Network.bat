c7a728c0df93c0ea8b120124211a46d25eba1ff3
gitcharcha <98260982+gitcharcha@users.noreply.github.com>
2022-02-05 17:38:19 +0530

    r2

--- Network.bat
+++
@@ -1,6 +0,0 @@
-:: This batch file checks for network connection problems.
-ECHO OFF
-:: View network connection details
-ipconfig /all
-:: Check if geeksforgeeks.com is reachable
-ping geeksforgeeks.com
